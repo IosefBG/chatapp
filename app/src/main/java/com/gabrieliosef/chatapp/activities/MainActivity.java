c4a07b4c5a7e89f652a0a6aa522ecd73432488d0
IosefBG <iosefgabriel268@gmail.com>
2023-01-15 16:53:45 +0200

    register works, need storage permissions

--- app/src/main/java/com/gabrieliosef/chatapp/activities/MainActivity.java
+++ app/src/main/java/com/gabrieliosef/chatapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.gabrieliosef.chatapp;
+package com.gabrieliosef.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.gabrieliosef.chatapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
